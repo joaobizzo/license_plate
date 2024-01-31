2498ceefde2da059fdac2526f01f4c6a294a9bd6
Joao Bizzo <brandtjoao7@gmail.com>
2024-01-31 15:13:04 -0300

    update

---
+++ main.py
@@ -0,0 +1,21 @@
+# load models
+
+# load video
+
+# read frames
+
+# detect vehicles
+
+# track vehicles
+
+# detecd license plates
+
+# assign license plates to car
+
+# crop license plate
+
+# process license plate
+
+# read license plate number
+
+# write results
