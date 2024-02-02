e043c51e3c1ec5cc8edc949506ff3f228fa7f133
Joao Bizzo <brandtjoao7@gmail.com>
2024-02-02 11:40:07 -0300

    update

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from ultralytics import YOLO
+import cv2
 
 
 # load models
@@ -6,22 +7,26 @@ coco_model = YOLO('yolov8n.pt')
 license_plate_detector = YOLO('models/license_plate_recognizer.pt')
 
 # load video
-
+cap = cv2.VideoCapture('cars.driving_1.mkv')
 
 # read frames
+ret = True
+while ret:
+    ret, frames = cap.read()
+    if ret:
+        pass
+        # detect vehicles
 
-# detect vehicles
+        # track vehicles
 
-# track vehicles
+        # detecd license plates
 
-# detecd license plates
+        # assign license plates to car
 
-# assign license plates to car
+        # crop license plate
 
-# crop license plate
+        # process license plate
 
-# process license plate
+        # read license plate number
 
-# read license plate number
-
-# write results
+        # write results
