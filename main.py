c5456a35713c5a30a9b5ad27c95fdf1739734432
Joao Bizzo <brandtjoao7@gmail.com>
2024-02-02 11:37:40 -0300

    update

--- main.py
+++ main.py
@@ -3,9 +3,10 @@ from ultralytics import YOLO
 
 # load models
 coco_model = YOLO('yolov8n.pt')
-license_plate_detector = YOLO('yolov8n.pt')
+license_plate_detector = YOLO('models/license_plate_recognizer.pt')
 
 # load video
+
 
 # read frames
 
