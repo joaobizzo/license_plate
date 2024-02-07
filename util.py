62d307e1c332d40a3c85edb2395001debcf31ce5
Joao Bizzo <brandtjoao7@gmail.com>
2024-02-07 15:28:01 -0300

    update code

--- util.py
+++ util.py
@@ -62,6 +62,34 @@ def read_license_plate(license_plate_crop):
 
     return 0, 0
 
+
 def get_car(license_plate, vehicle_track_id):
+    """
+    Retrieve the vehicle coordinates and ID based on the license plate coordinates.
+
+    Args:
+        license_plate (tuple): Tuple containing the coordinates of the license plate (x1, y1, x2, y2, score, class_id).
+        vehicle_track_ids (list): List of vehicle track IDs and their corresponding coordinates.
+
+    Returns:
+        tuple: Tuple containing the vehicle coordinates (x1, y1, x2, y2) and ID.
+    """
+
+
+
+    foundIt = False
+    x1, y1, x2, y2, score, class_id = license_plate
+
+    for j in range(len(vehicle_track_id)):
+        xcar1, ycar1, xcar2, ycar2, car_id = vehicle_track_id[j]
+
+        if x1 > xcar1 and y1 > ycar1 and x2 < xcar2 and y2 < ycar2:
+            car_indx = j
+            foundIt = True
+            break
     
-    return 0, 0, 0, 0, 0
+    if foundIt:
+        return vehicle_track_id[car_indx]
+             
+
+    return -1, -1, -1, -1, -1
