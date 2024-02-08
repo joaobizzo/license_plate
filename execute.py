4dba4dde087e9fbcf5fbe2fad4d4de3a303493dc
Joao Bizzo <brandtjoao7@gmail.com>
2024-02-08 18:37:24 -0300

    update

--- execute.py
+++ execute.py
@@ -1,8 +1,17 @@
 import subprocess
 
+# List of python scripts to execute
+py_scripts = ["main.py", "visualize.py"]
+
 def execute(command):
     process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     out, err = process.communicate()
-    return out, err
+    return out.decode(), err.decode()
 
-execute("teste.py")
+
+# Execute each script
+for script in py_scripts:
+    out, err = execute(f"python {script}")
+    print(out)
+    print(err)
+    print("\n\n\n")
--- main.py
+++ main.py
@@ -79,7 +79,7 @@ interpolated_data = interpolate_bounding_boxes(data)
 
 # Write updated data to a new CSV file
 header = ['frame_nmr', 'car_id', 'car_bbox', 'license_plate_bbox', 'license_plate_bbox_score', 'license_number', 'license_number_score']
-with open(os.path.join(data_path, 'test_interpolated.csv', 'w', newline='')) as file:
+with open(os.path.join(data_path, 'test_interpolated.csv'), 'w', newline='') as file:
     writer = csv.DictWriter(file, fieldnames=header)
     writer.writeheader()
     writer.writerows(interpolated_data)
